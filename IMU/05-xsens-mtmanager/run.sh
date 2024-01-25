1e9503bb07653223b04d9c628721f8937967ea69
Kavin Palanichamy <pkavin6310@gmail.com>
2024-01-25 12:58:33 +0100

    Driver_Stable

--- IMU/05-xsens-mtmanager/run.sh
+++ IMU/05-xsens-mtmanager/run.sh
@@ -2,6 +2,7 @@
 docker run -it --rm \
 	--volume="$HOME/.Xauthority:/home/labuser/.Xauthority:rw" \
 	--volume="./imu-data-transmitter:/home/labuser/imu-data-transmitter:rw" \
+	--volume="./xda_public_cpp:/home/labuser/xda_public_cpp:rw" \
 	--env="DISPLAY" \
 	--net=host \
 	--device=/dev/ttyUSB0 \
